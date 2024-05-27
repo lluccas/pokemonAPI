c29e0b1459d634ca3d1effd13d30eb85360fbe19
luccas.torres <luccas.torres@castgroup.com.br>
2024-05-27 20:52:53 -0300

    criando service para consumir api

--- pokemonAPI/src/app/app.component.ts
+++ pokemonAPI/src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    // outros módulos
+  ],
+  // declarações, bootstrap, etc.
+})
+export class AppModule { }
 
 @Component({
   selector: 'app-root',
